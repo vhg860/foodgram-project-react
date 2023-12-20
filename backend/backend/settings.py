0141e56bd6d577057afe2823b254c19251ebf17c
Дмитрий Истомин <vhg860@gmail.com>
2023-12-20 20:31:46 +0300

    deploy2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,13 +107,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
