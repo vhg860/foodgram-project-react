8987a1b996f022533dd81bd3863980a64c514141
Дмитрий Истомин <vhg860@gmail.com>
2023-12-21 15:54:19 +0300

    image try1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,10 +108,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
