7db20fc15bdf58152fb0deb03a1f5ecba8d1a834
Дмитрий Истомин <vhg860@gmail.com>
2023-12-22 15:08:38 +0300

    6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_key')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
